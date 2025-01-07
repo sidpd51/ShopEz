6add23bef2d8c9276709099eb5b24a0f8633f42b
siddharthpd <sidpd510@gmail.com>
2025-01-07 16:52:09 +0530

    feat: Add onchange event attribute on Field component

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -5,16 +5,17 @@ type fieldProps = {
     label: string,
     type: string,
     placeholder: string,
-    required?: boolean
+    required?: boolean,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const Field = ({ fieldname, label, type, placeholder, required = false }: fieldProps) => {
+const Field = ({ fieldname, label, type, placeholder, required = false, onChange }: fieldProps) => {
     return (
         <div>
             <div className="mb-2 block">
                 <Label htmlFor={fieldname} value={label} />
             </div>
-            <TextInput required={required} id={fieldname} type={type} placeholder={placeholder} />
+            <TextInput required={required} id={fieldname} type={type} placeholder={placeholder} onChange={onChange} />
         </div>
     )
 }
