109d44eb66426b6b61f9f08ab59b05530ad71d84
siddharthpd <sidpd510@gmail.com>
2025-01-08 11:33:04 +0530

    chore: update variables names

--- src/validationSchemas/auth.ts
+++ src/validationSchemas/auth.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const signUpSchema = z.object({
+const signupFormSchema = z.object({
     firstname: z.string().min(2).max(255),
     lastname: z.string().min(2).max(255),
     email: z.string().email(),
@@ -9,12 +9,12 @@ const signUpSchema = z.object({
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const signInSchema = z.object({
+const signinFormSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6).max(255),
 });
 
 
-export type SignUpType = z.infer<typeof signUpSchema>;
-export type SignInType = z.infer<typeof signInSchema>;
+export type SignupForm = z.infer<typeof signupFormSchema>;
+export type SigninForm = z.infer<typeof signinFormSchema>;
 
