c0c19202a327b0d75a09fc096569c334558c0c1a
siddharthpd <sidpd510@gmail.com>
2025-01-08 15:58:41 +0530

    feat: Update Tailwind CSS configuration for improved content and plugin integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,9 @@ const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // ...
-    flowbite.content(),
-  ],
-  plugins: [
-    // ...
-    flowbite.plugin(),
-  ],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+    theme: {
+        extend: {},
+    },
+    plugins: [flowbite.plugin()],
 };
