b2bc10a8ebf9c7b20c7c1f04f50a29c7796c7250
siddharthpd <sidpd510@gmail.com>
2025-01-08 13:08:51 +0530

    feat: update the field component with input

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,4 +1,5 @@
-import { Label, TextInput } from 'flowbite-react'
+import { Label } from "flowbite-react";
+import { UseFormRegister } from "react-hook-form";
 
 type fieldProps = {
     fieldname: string,
@@ -6,16 +7,22 @@ type fieldProps = {
     type: string,
     placeholder: string,
     required?: boolean,
-    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    registerField: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    register: UseFormRegister<any>
 }
 
-const Field = ({ fieldname, label, type, placeholder, required = false, onChange }: fieldProps) => {
+const Field = ({ fieldname, label, type, placeholder, registerField, register, required }: fieldProps) => {
     return (
         <div>
-            <div className="mb-2 block">
-                <Label htmlFor={fieldname} value={label} />
-            </div>
-            <TextInput required={required} id={fieldname} type={type} placeholder={placeholder} onChange={onChange} />
+            <Label htmlFor={fieldname} value={label} />
+            <input {...register(registerField)}
+                type={type}
+                name={fieldname}
+                id={fieldname}
+                className="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                placeholder={placeholder} required={required} />
         </div>
     )
 }
