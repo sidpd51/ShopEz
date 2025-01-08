ba55d42173b248e936d980d02c51a04b63b713ee
sidpd51 <sidpd510@gmail.com>
2025-01-08 21:52:30 +0530

    feat: Enhance ProductCard layout for better responsiveness and visual appeal

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,15 +7,13 @@ type ProductCardProps = {
 
 const ProductCard = ({ title, price, image, rating }: ProductCardProps) => {
     return (
-
-
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img className="p-8 rounded-t-lg" src={image} />
+        <div className="w-full xl:w-3/4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#" >
+                <img className="p-4 rounded-t-lg xl:w-3/4 mx-auto" src={image} />
             </a>
             <div className="px-5 pb-5">
                 <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+                    <h5 className="xl:text-sm font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
                     <div className="flex items-center space-x-1 rtl:space-x-reverse">
@@ -38,7 +36,7 @@ const ProductCard = ({ title, price, image, rating }: ProductCardProps) => {
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">{rating}</span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                    <span className="xl:text-xl font-bold text-gray-900 dark:text-white">${price}</span>
                     <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                 </div>
             </div>
